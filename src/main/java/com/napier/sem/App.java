52ab8646ab9c8653615dd1a8734a0eca72fc6a80
Johvahn brodie <johvahn14@gmail.com>
2024-04-22 00:36:57 -0400

    Release (#39)
    * Adding develop build status to GITHub
    
    * Formatting Of Screenshot
    
    * MongoDB
    
    * MongoDB Server
    
    * DB Is Now Connected
    
    * DB Is Now Connected1
    
    * Single JAR for deployment
    
    * New Image Connecting to MongoDB
    
    * connect to SQL DB
    
    * Update  GitHub Action file
    
    * Use cases (#33)
    
    * Added Use Case 1
    
    * use-case-2
    
    * Added use-case-3
    
    * Added use-case-3
    
    * Added use-case-3
    
    * Added Use Case 4,5,6,7
    
    * Added Use Case 4,5,6,7
    
    * Added Use Case 9,10
    
    * Added Use Case 11 to 14
    
    * Added Use Case 15 to 19
    
    * Added Use Case 19 to 21
    
    * Added Use Case 22
    
    * Added Use Case 23 to 25
    
    ---------
    
    Co-authored-by: nutellayen <157748332+nutellayan@users.noreply.github.com>
    
    * Add use diagram
    
    * Develop (#38)
    
    * Print Countries (#35)
    
    * Print Continent Countries (#36)
    
    * Print Region Countries (#37)
    
    * Update App.java
    
    ---------
    
    Co-authored-by: Otega Alexander Taire <40677824@live.napier.ac.uk>
    Co-authored-by: nutellayen <157748332+nutellayan@users.noreply.github.com>
    Co-authored-by: Oi Yan <40677197@live.napier.ac.uk>

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -1,8 +1,8 @@
 package com.napier.sem;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
+
 
 public class App
 {
@@ -15,8 +15,7 @@ public class App
         a.connect();
 
 
-
-
+        a.Init(a);
 
 
         // Disconnect from database
@@ -24,6 +23,164 @@ public class App
     }
 
     private Connection con = null;
+
+
+    public void Init(App a){
+
+        // Get All Countries From Largest to Smallest
+//        ArrayList<Country> countries1 = a.getAllCountries();
+
+
+        //Get ALl Countries in a Continent From Largest to Smallest
+//        ArrayList<Country> countries2 = a.getCountriesByContinent("Asia");
+
+
+        //Get ALl Countries in a Region From Largest to Smallest
+        ArrayList<Country> countries3 = a.getCountriesByRegion("Caribbean");
+
+    }
+
+
+
+
+    //Query Functions//
+    public ArrayList<Country> getAllCountries() {
+        try {
+            // Create an SQL statement
+            Statement stmt = con.createStatement();
+            // Create string for SQL statement
+            String strSelect =
+                    "SELECT * FROM country "
+                            + "ORDER BY population DESC"; // Ordering by population in descending order
+            // Execute SQL statement
+            ResultSet rset = stmt.executeQuery(strSelect);
+            // Extract country information
+            ArrayList<Country> countries = new ArrayList<>();
+            while (rset.next()) {
+                // Assuming Country class has a constructor that takes in the necessary parameters
+                Country country = new Country();
+                country.code = rset.getString("Code");
+                country.name = rset.getString("Name");
+                country.continent = rset.getString("Continent");
+                country.region = rset.getString("Region");
+                country.population = rset.getInt("Population");
+
+                countries.add(country);
+            }
+
+            printCountries(countries);
+
+            return countries;
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get country details");
+            return null;
+        }
+    }
+
+
+
+    public ArrayList<Country> getCountriesByContinent(String continent) {
+        try {
+            // Create SQL statement
+            String sql = "SELECT * FROM country WHERE Continent = ? ORDER BY Population DESC";
+            PreparedStatement stmt = con.prepareStatement(sql);
+            stmt.setString(1, continent);
+
+            // Execute query
+            ResultSet rs = stmt.executeQuery();
+
+            // Process result set
+            ArrayList<Country> countries = new ArrayList<>();
+            while (rs.next()) {
+                Country country = new Country();
+                country.code = rs.getString("Code");
+                country.name = rs.getString("Name");
+                country.continent = rs.getString("Continent");
+                country.region = rs.getString("Region");
+                country.surfaceArea = rs.getDouble("SurfaceArea");
+                country.indepYear = rs.getInt("IndepYear");
+                country.population = rs.getInt("Population");
+                country.capital = rs.getInt("Capital");
+                countries.add(country);
+            }
+
+            // Close resources
+            rs.close();
+            stmt.close();
+
+            printCountries(countries);
+
+            return countries;
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get country details");
+            return null;
+        }
+    }
+
+
+    public ArrayList<Country> getCountriesByRegion(String region) {
+        try {
+            // Create SQL statement
+            String sql = "SELECT * FROM country WHERE Region = ? ORDER BY Population DESC";
+            PreparedStatement stmt = con.prepareStatement(sql);
+            stmt.setString(1, region);
+
+            // Execute query
+            ResultSet rs = stmt.executeQuery();
+
+            // Process result set
+            ArrayList<Country> countries = new ArrayList<>();
+            while (rs.next()) {
+                Country country = new Country();
+                country.code = rs.getString("Code");
+                country.name = rs.getString("Name");
+                country.continent = rs.getString("Continent");
+                country.region = rs.getString("Region");
+                country.surfaceArea = rs.getDouble("SurfaceArea");
+                country.indepYear = rs.getInt("IndepYear");
+                country.population = rs.getInt("Population");
+                country.capital = rs.getInt("Capital");
+                countries.add(country);
+            }
+
+            // Close resources
+            rs.close();
+            stmt.close();
+
+            printCountries(countries);
+
+            return countries;
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get country details");
+            return null;
+        }
+    }
+
+
+
+
+
+
+    //Print Function//
+    public void printCountries(ArrayList<Country> countries) {
+        // Print header
+        System.out.println(String.format("%-5s %-52s %-15s %-26s %-10s", "Code", "Name", "Continent", "Region", "Population"));
+        // Loop over all countries in the list
+        for (Country country : countries) {
+            String countryString =
+                    String.format("%-5s %-52s %-15s %-26s %,10d",
+                            country.code, country.name, country.continent, country.region, country.population);
+            System.out.println(countryString);
+        }
+    }
+
+
+
+    //Connection Functions//
 
     public void connect()
     {
@@ -47,7 +204,7 @@ public class App
                 // Wait a bit for db to start
                 Thread.sleep(10000);
                 // Connect to database
-                con = DriverManager.getConnection("jdbc:mysql://localhost:3306/world?useSSL=false", "root", "example");
+                con = DriverManager.getConnection("jdbc:mysql://db:3306/world?useSSL=false", "root", "example");
                 System.out.println("Successfully connected");
                 break;
             }
@@ -62,6 +219,8 @@ public class App
             }
         }
     }
+
+
 
     public void disconnect()
     {
