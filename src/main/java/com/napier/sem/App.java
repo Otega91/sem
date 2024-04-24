a07fcce56acb14b5743a3dd8a2e583de0b19dc25
Johvahn brodie <johvahn14@gmail.com>
2024-04-24 07:53:48 -0400

    Develop (#41)
    * Adding develop build status to GITHub
    
    * Formatting Of Screenshot
    
    * MongoDB
    
    * MongoDB Server
    
    * DB Is Now Connected
    
    * DB Is Now Connected1
    
    * Single JAR for deployment
    
    * New Image Connecting to MongoDB
    
    * connect to SQL DB
    
    * Update  GitHub Action file
    
    * Use cases (#33)
    
    * Added Use Case 1
    
    * use-case-2
    
    * Added use-case-3
    
    * Added use-case-3
    
    * Added use-case-3
    
    * Added Use Case 4,5,6,7
    
    * Added Use Case 4,5,6,7
    
    * Added Use Case 9,10
    
    * Added Use Case 11 to 14
    
    * Added Use Case 15 to 19
    
    * Added Use Case 19 to 21
    
    * Added Use Case 22
    
    * Added Use Case 23 to 25
    
    ---------
    
    Co-authored-by: nutellayen <157748332+nutellayan@users.noreply.github.com>
    
    * Add use diagram
    
    * Print Countries (#35)
    
    * Print Continent Countries (#36)
    
    * Print Region Countries (#37)
    
    * Print N Countries (#40)
    
    * Updated the read me file
    
    ---------
    
    Co-authored-by: Otega Alexander Taire <40677824@live.napier.ac.uk>
    Co-authored-by: nutellayen <157748332+nutellayan@users.noreply.github.com>
    Co-authored-by: Oi Yan <40677197@live.napier.ac.uk>

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -3,7 +3,6 @@ package com.napier.sem;
 import java.sql.*;
 import java.util.ArrayList;
 
-
 public class App
 {
     public static void main(String[] args)
@@ -37,6 +36,15 @@ public class App
 
         //Get ALl Countries in a Region From Largest to Smallest
         ArrayList<Country> countries3 = a.getCountriesByRegion("Caribbean");
+
+
+        //The top N populated countries in the world.
+        ArrayList<Country> countries4 = a.getTopPopulatedCountries(5);
+
+
+        //The top N populated countries in a continent
+//        ArrayList<Country> countries5 = a.getTopPopulatedCountriesInContinent("Caribbean",5);
+
 
     }
 
@@ -80,7 +88,6 @@ public class App
     }
 
 
-
     public ArrayList<Country> getCountriesByContinent(String continent) {
         try {
             // Create SQL statement
@@ -99,8 +106,6 @@ public class App
                 country.name = rs.getString("Name");
                 country.continent = rs.getString("Continent");
                 country.region = rs.getString("Region");
-                country.surfaceArea = rs.getDouble("SurfaceArea");
-                country.indepYear = rs.getInt("IndepYear");
                 country.population = rs.getInt("Population");
                 country.capital = rs.getInt("Capital");
                 countries.add(country);
@@ -139,8 +144,6 @@ public class App
                 country.name = rs.getString("Name");
                 country.continent = rs.getString("Continent");
                 country.region = rs.getString("Region");
-                country.surfaceArea = rs.getDouble("SurfaceArea");
-                country.indepYear = rs.getInt("IndepYear");
                 country.population = rs.getInt("Population");
                 country.capital = rs.getInt("Capital");
                 countries.add(country);
@@ -161,23 +164,128 @@ public class App
     }
 
 
+    public ArrayList<Country> getTopPopulatedCountries(int N) {
+        try {
+            // Create SQL statement
+            String sql = "SELECT * FROM country ORDER BY Population DESC LIMIT ?";
+            PreparedStatement stmt = con.prepareStatement(sql);
+            stmt.setInt(1, N);
 
+            // Execute query
+            ResultSet rs = stmt.executeQuery();
 
+            // Process result set
+            ArrayList<Country> countries = new ArrayList<>();
+            while (rs.next()) {
+                Country country = new Country();
+                country.code = rs.getString("Code");
+                country.name = rs.getString("Name");
+                country.continent = rs.getString("Continent");
+                country.region = rs.getString("Region");
+                country.population = rs.getInt("Population");
+                country.capital = rs.getInt("Capital");
+                countries.add(country);
+            }
 
+            // Close resources
+            rs.close();
+            stmt.close();
 
-    //Print Function//
-    public void printCountries(ArrayList<Country> countries) {
-        // Print header
-        System.out.println(String.format("%-5s %-52s %-15s %-26s %-10s", "Code", "Name", "Continent", "Region", "Population"));
-        // Loop over all countries in the list
-        for (Country country : countries) {
-            String countryString =
-                    String.format("%-5s %-52s %-15s %-26s %,10d",
-                            country.code, country.name, country.continent, country.region, country.population);
-            System.out.println(countryString);
+            printCountries(countries);
+
+            return countries;
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get country details");
+            return null; // Handle the exception according to your application's requirements
         }
     }
 
+
+    public ArrayList<Country> getTopPopulatedCountriesInContinent(String continent, int N) {
+        try {
+            // Create SQL statement
+            String sql = "SELECT * FROM country WHERE Continent = ? ORDER BY Population DESC LIMIT ?";
+            PreparedStatement stmt = con.prepareStatement(sql);
+            stmt.setString(1, continent);
+            stmt.setInt(2, N);
+
+            // Execute query
+            ResultSet rs = stmt.executeQuery();
+
+            // Process result set
+            ArrayList<Country> countries = new ArrayList<>();
+            while (rs.next()) {
+                Country country = new Country();
+                country.code = rs.getString("Code");
+                country.name = rs.getString("Name");
+                country.continent = rs.getString("Continent");
+                country.region = rs.getString("Region");
+                country.population = rs.getInt("Population");
+                country.capital = rs.getInt("Capital");
+                countries.add(country);
+            }
+
+            // Close resources
+            rs.close();
+            stmt.close();
+
+            printCountries(countries);
+
+
+            return countries;
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get country details");
+            return null; // Handle the exception according to your application's requirements
+        }
+    }
+
+    //Dependent Query Function//
+    private int getTotalCityPopulation(String countryCode) {
+        try {
+            // Create SQL statement
+            String sql = "SELECT Population FROM city WHERE CountryCode = ?";
+            PreparedStatement stmt = con.prepareStatement(sql);
+            stmt.setString(1, countryCode);
+
+            // Execute query
+            ResultSet rs = stmt.executeQuery();
+
+            // Process result set
+            if (rs.next()) {
+                return rs.getInt("Population");
+            } else {
+                return 0; // Return 0 if no city population found for the country
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get city details");
+            return 0; // Handle the exception according to your application's requirements
+        }
+    }
+
+
+    //Print Function//
+
+    public void printCountries(ArrayList<Country> countries) {
+        // Print header
+        System.out.println(String.format("%-52s %-15s %-26s %-20s %-20s %-12s %-12s", "Name", "Continent", "Region", "Total Population", "City Population", "Living City %", "Not Living City %"));
+        // Loop over all countries in the list
+        for (Country country : countries) {
+            int totalPopulation = country.population;
+            int cityPopulation = getTotalCityPopulation(country.code);
+            int ruralPopulation = totalPopulation - cityPopulation;
+            double cityPercentage = ((double) cityPopulation / totalPopulation) * 100;
+            double ruralPercentage = 100 - cityPercentage;
+
+            String countryString =
+                    String.format("%-52s %-15s %-26s %,20d %,20d %,12f%% %,12f%%",
+                            country.name, country.continent, country.region, totalPopulation, cityPopulation, cityPercentage, ruralPercentage);
+
+            System.out.println(countryString);
+        }
+    }
 
 
     //Connection Functions//
@@ -220,8 +328,6 @@ public class App
         }
     }
 
-
-
     public void disconnect()
     {
         if (con != null)
@@ -239,3 +345,4 @@ public class App
     }
 
 }
+
---
+++ src/main/java/com/napier/sem/City.java
@@ -0,0 +1,8 @@
+package com.napier.sem;
+
+public class City {
+
+    public String name;
+    public String country;
+    public int population;
+}
